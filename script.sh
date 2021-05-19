00744a42ca5887509419a40e8f2bb2e5c558f753
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-05-19 10:17:26 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -4,7 +4,7 @@ sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add 
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
 
-sudo cat << 'EOF' > /etc/docker/daemon.json
+sudo cat << EOF > /etc/docker/daemon.json
 {
  "exec-opts": ["native.cgroupdriver=systemd"],
  "log-driver": "json-file",
