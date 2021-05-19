cb7c08149e9fb376e95130ae4c662ff983854537
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-05-19 10:03:27 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -3,3 +3,34 @@ sudo apt-get update
 sudo curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
 sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu $(lsb_release -cs) stable"
 sudo apt-get install -y docker-ce docker-ce-cli containerd.io
+
+sudo cat << 'EOF' > /etc/docker/daemon.json
+{
+ "exec-opts": ["native.cgroupdriver=systemd"],
+ "log-driver": "json-file",
+ "log-opts": {
+ "max-size": "100m"
+ },
+ "storage-driver": "overlay2"
+}
+EOF
+
+sudo mkdir -p /etc/systemd/system/docker.service.d
+sudo systemctl daemon-reload
+sudo systemctl restart docker
+
+sudo apt-get install -y apt-transport-https
+sudo curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | sudo apt-key add -
+sudo cat <<EOF | sudo tee /etc/apt/sources.list.d/kubernetes.list deb https://apt.kubernetes.io/ kubernetesxenial main 
+EOF
+
+sudo cat <<EOF > /etc/sysctl.d/k8s.conf
+net.bridge.bridge-nf-call-ip6tables = 1
+net.bridge.bridge-nf-call-iptables = 1
+EOF
+
+sudo sysctl --system
+sudo lsmod | grep br_netfilter
+sudo apt-get update
+sudo apt-get install -y kubelet kubeadm Kubectl
+sudo apt-mark hold kubelet kubeadm Kubectl
