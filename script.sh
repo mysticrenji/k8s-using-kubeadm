19e88ad39400b5f6bed9c755f9432c0f13cbc4cd
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-05-30 13:09:10 +0800

    Update script.sh

--- script.sh
+++ script.sh
@@ -33,7 +33,7 @@ sudo lsmod | grep br_netfilter
 sudo sh -c "echo 'deb http://apt.kubernetes.io/ kubernetes-xenial main' >> /etc/apt/sources.list.d/kubernetes.list"
 sudo sh -c "curl -s https://packages.cloud.google.com/apt/doc/apt-key.gpg | apt-key add -"
 sudo apt-get update
-sudo apt-get install -y kubeadm=1.21.1-00  kubelet=1.21.1-00  kubectl=1.21.1-00 
+sudo apt-get install -y kubeadm=1.20.1-00  kubelet=1.20.1-00  kubectl=1.20.1-00 
 sudo apt-mark hold kubelet kubeadm kubectl
 
 sudo swapoff -a
