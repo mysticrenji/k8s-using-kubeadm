b30c41bbb22c99938c0f785af3a6ed1844e9ae96
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2021-10-22 14:50:22 +0000

    Add main file

--- outputs.tf
+++ outputs.tf
@@ -1,21 +1,2 @@
 
-output "client_secret" {
-  value     = module.serviceprincipal.client_secret
-  sensitive = true
-}
-
-output "client_id" {
-  value     = module.serviceprincipal.client_id
-  sensitive = true
-}
-
-output "service_principal_application_id" {
-  value     = module.serviceprincipal.service_principal_application_id
-  sensitive = true
-}
-
-output "service_principal_object_id" {
-  value     = module.serviceprincipal.service_principal_object_id
-  sensitive = true
-}
 
