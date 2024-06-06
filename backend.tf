7dcc68036ae642d81c17903ac9daa8fe89f81756
Renjith Ravindranathan <mysticrenji@users.noreply.github.com>
2024-06-06 20:36:00 +0200

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "azurerm" {
     resource_group_name  = "sandbox-storage"
-    storage_account_name = "mystickenshin"
+    storage_account_name = "az_storage_box"
     container_name       = "terraform"
     key                  = "terraform.tfstate"
 
